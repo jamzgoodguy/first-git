ea102b2352aee811a83cbfc7a225e6c1eb0dd6ca
jamzgoodguy <jamilahgua@gmail.com>
2016-11-29 10:58:46 +0700

    yes offcour

---
+++ may.js
@@ -0,0 +1,8 @@
+(function() {
+var div = document.createElement('div');
+div.className = 'amirkepo';
+        div.innerHTML = '<b>cokdeh</b>';
+var head = document.getElementById('sidebar-right-1');
+
+head.appendChild(div);
+})();

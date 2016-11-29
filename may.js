4c8af05ee3768803a5d2c83055584617d685db33
jamzgoodguy <jamilahgua@gmail.com>
2016-11-29 11:14:10 +0700

    Update may.js

--- may.js
+++ may.js
@@ -1,4 +1,15 @@
-function replaceContentInContainer(target, source) {
-      document.getElementById(target).innerHTML = document.getElementById(source).innerHTML;
-   }
-replaceContentInContainer('BlogArchive1', 'Profile1');
+var iDiv = document.createElement('div');
+iDiv.id = 'block';
+iDiv.className = 'block';
+document.getElementsByTagName('body')[0].appendChild(iDiv);
+
+iDiv.innerHTML = "I'm the first div";
+
+// Now create and append to iDiv
+var innerDiv = document.createElement('div');
+innerDiv.className = 'block-2';
+
+// The variable iDiv is still good... Just append to it.
+iDiv.appendChild(innerDiv);
+innerDiv.innerHTML = "I'm the inner div";
+
